1628e781c14ed8e7f16edefccd24803ee38d66d0
Jaspreet Singh <harshitbhattal@gmail.com>
2022-10-12 19:05:29 +0530

    nav update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import classes from "./App.css";
-import Mainnavbar from "./Layout/MainNavbar";
 import "./index.css";
 import MainNavbar from "./Layout/MainNavbar";
 function App() {
