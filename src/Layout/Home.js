8763213570fb6ced806518c96c9bae32e8eb7696
Jaspreet Singh <harshitbhattal@gmail.com>
2022-10-12 20:03:31 +0530

    home creation

--- src/Layout/Home.js
+++ src/Layout/Home.js
@@ -3,7 +3,19 @@ import ReactDOM from "react-dom";
 import classes from "../Style/Home.css";
 function Home() {
     return(
-        <div>hii</div>
+        <section class="main">
+            <div class="main-sec">
+                <h1 class="heading">
+                    We're<br></br>
+                    connecting<br></br>
+                    techies<br></br>
+                    together
+                </h1>
+            </div>
+            <div class="main-th">
+
+            </div>
+        </section>
     );
 }
 export default Home;
