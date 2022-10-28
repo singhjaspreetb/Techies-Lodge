1333926aa8c78cd7f608057aed6d19bea5f5a794
Rohit23032003 <sohitrohit1717@gmail.com>
2022-10-28 21:29:20 +0530

    created signup

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Routes , Route } from 'react-router-dom';
+function Signup() {
+    return (
+        <div>Signup page</div>
+
+    );
+}
+
+export default Signup;
