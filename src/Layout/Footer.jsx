a11fe6b4c3dbaa76754a0a542af2835a934368b2
ABHINAY-31 <rsingh9766@gmail.com>
2022-10-28 21:59:16 +0530

    Added footer page

---
+++ src/Layout/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Routes , Route } from 'react-router-dom';
+function Footer() {
+    return (
+        <div>Footer page</div>
+
+    );
+}
+
+export default Footer;
