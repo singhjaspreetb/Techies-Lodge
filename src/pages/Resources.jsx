b8356cc464bbaced4df1efea294f2924d6f06441
Jaspreet Singh <harshitbhattal@gmail.com>
2022-11-10 18:58:28 +0530

    footer and resourses

--- src/pages/Resources.jsx
+++ src/pages/Resources.jsx
@@ -1,7 +1,111 @@
 import React from "react";
+import Footer from "../Layout/Footer";
+import classes from "../Style/Resources.css"
+import info_img from "../assets/image/android.png"
+
 function Resources() {
     return (
-        <div>Resources page</div>
+        <div>
+            <div class="row ml-5 mb-5 mb-5 mt-5">
+                <div class="container col-lg-4">
+                    <div class="card">
+                        <div class="imgBx">
+                            <img src={info_img} />
+                        </div>
+                        <div class="contentBx">
+                            <h2>Android</h2>
+                            {/* <div class="size">
+                                <h3>Size :</h3>
+                                <span>7</span>
+                                <span>8</span>
+                                <span>9</span>
+                                <span>10</span>
+                            </div>
+                            <div class="color">
+                                <h3>Color :</h3>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </div> */}
+                            <a class="mt-5" href="#">See Roadmap</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="container col-lg-4">
+                    <div class="card">
+                        <div class="imgBx">
+                            <img src={info_img} />
+                        </div>
+                        <div class="contentBx">
+                            <h2>Android</h2>
+                            <a class="mt-5" href="#">See Roadmap</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="container col-lg-4">
+                    <div class="card">
+                        <div class="imgBx">
+                            <img src={info_img} />
+                        </div>
+                        <div class="contentBx">
+                            <h2>Android</h2>
+                            <a class="mt-5" href="#">See Roadmap</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row ml-5 mb-5 mb-5 mt-5">
+                <div class="container col-lg-4">
+                    <div class="card">
+                        <div class="imgBx">
+                            <img src={info_img} />
+                        </div>
+                        <div class="contentBx">
+                            <h2>Android</h2>
+                            {/* <div class="size">
+                                <h3>Size :</h3>
+                                <span>7</span>
+                                <span>8</span>
+                                <span>9</span>
+                                <span>10</span>
+                            </div>
+                            <div class="color">
+                                <h3>Color :</h3>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </div> */}
+                            <a class="mt-5" href="#">See Roadmap</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="container col-lg-4">
+                    <div class="card">
+                        <div class="imgBx">
+                            <img src={info_img} />
+                        </div>
+                        <div class="contentBx">
+                            <h2>Android</h2>
+                            <a class="mt-5" href="#">See Roadmap</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="container col-lg-4">
+                    <div class="card">
+                        <div class="imgBx">
+                            <img src={info_img} />
+                        </div>
+                        <div class="contentBx">
+                            <h2>Android</h2>
+                            <a class="mt-5" href="#">See Roadmap</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="mt-5">
+                <Footer />
+            </div>
+        </div>
 
     );
 }
