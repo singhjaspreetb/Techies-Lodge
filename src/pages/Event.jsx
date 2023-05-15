9cf9910d97649a6efc59150512e0ab1183975884
raipratik <pratikkumar399@gmail.com>
2023-05-15 10:54:47 +0530

    fixed the routing

--- src/pages/Event.jsx
+++ src/pages/Event.jsx
@@ -3,13 +3,13 @@ import styles from "../style";
 import CTA from "../components/CTA"
 function Event() {
     return (
-        <div>
+        <>
             <div className="flex min-h-screen items-center justify-center bg-black-800">
                 <div className={`${styles.boxWidth}`}>
                     <HeroSection />
                 </div>
             </div>
-        </div>
+        </>
     );
 }
 
