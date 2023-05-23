6375b3d0b80c684cc80fdcf4781e662342197c66
raipratik <pratikkumar399@gmail.com>
2023-05-24 03:18:27 +0530

    .

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,11 +2,11 @@ import { people01, people02, people03, facebook, instagram, linkedin, twitter, a
 
 export const navLinks = [
   {
-    id: "home",
+    id: "/home",
     title: "Home",
   },
   {
-    id: "events",
+    id: "/events",
     title: "Events",
   },
   {
